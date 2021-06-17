dee619205ea5cab4bc17e510a3b723877f26a2f0
amir <zareamirhussein@gmail.com>
2021-06-18 03:45:34 +0430

    adding header layout

--- src/layouts/Header/Header.layout.js
+++ src/layouts/Header/Header.layout.js
@@ -1,35 +1,156 @@
-import {Link} from "react-router-dom"
-import styles from "./Header.module.scss"
-import { ReactComponent as ShoppingIcon } from "../../assets/icons/shop-logo.svg"
-import { ReactComponent as ShoppingCartIcon } from "../../assets/icons/shopping-cart.svg"
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MailIcon from '@material-ui/icons/Mail';
+import MoreIcon from '@material-ui/icons/MoreVert';
 
-function Header(){
-    // cartNumber = 0
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
 
-    return (
-        <header className={styles['main-header']}>
-            <div className={styles['main-header--logo']}>
-                <h1>فروشگاه فلان</h1>
-                <div className={styles['main-header--logo__icon']}><ShoppingIcon/></div>
-            </div>
-            <nav className={styles['main-navigation']}>
-                <ul>
-                    <li>
-                        <div className={styles['cart-container']}>
-                            <span className={styles['cart-count']}>0</span>
-                            <span className={styles['cart-text']}>سبد خرید</span>
-                            <div className={styles['cart-icon']}><ShoppingCartIcon/></div>
-                        </div>
-                    </li>
-                    <li>
-                        <a>مدیریت</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+export default function Header() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+
+        <MenuItem  >
+        <IconButton aria-label="show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+            </Badge>
+            </IconButton>
+            <p>سبد خرید</p>
+        </MenuItem>
+        <MenuItem>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+            </Badge>
+            </IconButton>
+            <p>مدیریت</p>
+        </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+            <MenuItem>
+                <Typography className={classes.title} variant="p" noWrap>
+                    سبد خرید<MailIcon/>
+                </Typography>
+            </MenuItem>
+            <MenuItem>
+                <Typography className={classes.title} variant="p" noWrap>
+                    مدیریت
+                </Typography>
+            </MenuItem>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <MenuItem>
+                <Typography className={classes.title} variant="h6" noWrap>
+                    فروشگاه فلان
+                </Typography>
+            </MenuItem>
+            <MenuItem>
+                <Typography className={classes.title} variant="h6" noWrap>
+                    <MailIcon/>
+                </Typography>
+            </MenuItem>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
 }
 
-export {
-    Header
-}
+export {Header}
