7464c63dd3d3dbf2b912ea3e3bb7deeaf674cdd8
amir <zareamirhussein@gmail.com>
2021-06-17 20:48:45 +0430

    finishing admin panel header

--- src/layouts/PanelHeader/PanelHeader.layout.js
+++ src/layouts/PanelHeader/PanelHeader.layout.js
@@ -1,15 +1,21 @@
 import styles from "./PanelHeader.module.scss"
+import {ReactComponent as Logo} from "../../assets/icons/logo.svg"
 
 function PanelHeader(){
     return (
         <header className={styles['panel-header']}>
-            <h1>پنل مدیریت فروشگاه</h1>
-            <ul>
-                <li>کالا ها</li>
-                <li>موجودی</li>
-                <li>قیمت ها</li>
-            </ul>
-            <div><a href="#">بازگشت به سایت</a></div>
+            <div className={styles['header-logo']}>
+                <h1>پنل مدیریت فروشگاه</h1>
+                <div>{<Logo className={styles['logo']}/>}</div>
+            </div>
+            <nav className={styles['header-navigation']}>
+                <div><a href="#">بازگشت به سایت</a></div>   
+                <ul>
+                    <li>کالا ها</li>
+                    <li>موجودی</li>
+                    <li>قیمت ها</li>
+                </ul>
+            </nav>
         </header>
     )
 }
