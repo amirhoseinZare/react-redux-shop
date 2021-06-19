32ae58a136eb73805b2af72471df8949b760fdfd
amir <zareamirhussein@gmail.com>
2021-06-19 17:45:59 +0430

    panel login page finished

--- src/pages/panel/panel-login-page/PanelLogin.page.js
+++ src/pages/panel/panel-login-page/PanelLogin.page.js
@@ -1,11 +1,35 @@
-import {PanelHeader} from "../../../layouts/index"
+import { TextField, Button, Grid, Typography } from '@material-ui/core';
+import { Link } from "react-router-dom"
+import classes from "./PanelLogin.module.scss"
 
 function PanelLoginPage (){
     return (
         <div>
-        <PanelHeader/>
-        panel login page</div>
-    )
+            <Grid item lg={4} md={6} sm={6} xs={10} xl={3} className={classes.loginFormContainer}>
+                <Typography variant='h6' component='h1' className={classes.loginFormHeader}>ورود به پنل مدیریت فروشگاه</Typography>
+                <form>
+                    <div className={classes.loginInput}>
+                        <div className={classes.loginLabel}>
+                            <label>نام کاربری</label>
+                        </div>
+                        <TextField dir="rtl" placeholder="مثال : amir" type="text"/>
+                    </div>
+                    <div className={classes.loginInput}>
+                        <div className={classes.loginLabel}>
+                            <label>رمز عبور</label>
+                        </div>
+                        <TextField dir="rtl" placeholder="مثال : 1234" type="password"/>
+                    </div>
+                    <div className={classes.login‌Button} >
+                        <Button>ورود</Button>
+                    </div>
+                </form>
+                <div className={classes.backHome}>
+                    <Link to="/">بازگشت به سایت</Link>
+                </div>
+            </Grid>
+        </div>
+      );
 }
 
 export {PanelLoginPage}
