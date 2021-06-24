7580431b086bca980c2e7c594dc68daadca21055
amir <zareamirhussein@gmail.com>
2021-06-24 21:38:12 +0430

    adding button and typography to panel quantity page

--- src/pages/panel/panel-quantity-page/PanelQuantity.page.js
+++ src/pages/panel/panel-quantity-page/PanelQuantity.page.js
@@ -1,10 +1,30 @@
 import {PanelHeader} from "../../../layouts/index"
 import {QuantityTable} from "../../../components/index"
 
+import {Typography, Button, Grid} from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    container:{
+        margin:'20px auto 0',
+        display: 'flex',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+});
+
+
 function PanelQuantityPage (){
+    const classes = useStyles();
+
     return (
         <div>
             <PanelHeader/>
+            <Grid item lg={8} md={10} sm ={10} xs={10} className={classes.container}>
+                <Button variant="contained" color="primary">ذخیره</Button>
+                <Typography variant="h4" component="p">مدیریت موجودی و قیمت ها</Typography>
+            </Grid>
             <QuantityTable/>
         </div>
     )
