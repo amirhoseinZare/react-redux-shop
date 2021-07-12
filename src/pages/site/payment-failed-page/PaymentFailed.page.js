dfaa1c3a2694874c4590cdc10b34aa3cdbd89f2f
amir <zareamirhussein@gmail.com>
2021-07-12 23:46:05 +0430

    finishing all about payments results pages

--- src/pages/site/payment-failed-page/PaymentFailed.page.js
+++ src/pages/site/payment-failed-page/PaymentFailed.page.js
@@ -1,6 +1,7 @@
 import {Header} from "../../../layouts/index"
 import {Typography, Grid} from "@material-ui/core"
 import {makeStyles} from "@material-ui/core/styles"
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles((theme) => ({
     header:{
@@ -17,12 +18,21 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center'
     },
     paymentIcon:{
+        position:'relative',
         width:'100px', 
         height:'100px',
         borderRadius:'50%',
         backgroundColor:'red',
         marginLeft:'40px'
     },
+    paymentIconItem:{
+        fill: '#ffff',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        fontSize: 'xxx-large',
+        transform: 'translate(-50%, -50%)'
+    }
 
 }));
 
@@ -35,8 +45,8 @@ function PaymentFailedPage(){
             <Header/>
             <Typography variant="h4" className={classes.header}>نتیجه ی پرداخت</Typography>
             <Grid item lg={8} md={10} sm ={10} xs={10} className={classes.container}>
-                <div className={classes.paymentIcon}></div>
-                <Typography variant="p" component="p">نتیجه ی پرداخت</Typography>
+                <div className={classes.paymentIcon}><CloseIcon className={classes.paymentIconItem}/></div>
+                <Typography dir="rtl" variant="p" component="p">پرداخت موفقیت آمیز نبود. سفارش شما در انتظار پرداخت است.</Typography>
             </Grid>
         </div>
     )
--- src/pages/site/payment-success-page/PaymentSuccess.page.js
+++ src/pages/site/payment-success-page/PaymentSuccess.page.js
@@ -1,6 +1,7 @@
 import {Header} from "../../../layouts/index"
 import {Typography, Grid} from "@material-ui/core"
 import {makeStyles} from "@material-ui/core/styles"
+import DoneIcon from '@material-ui/icons/Done';
 
 const useStyles = makeStyles((theme) => ({
     header:{
@@ -17,11 +18,20 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center'
     },
     paymentIcon:{
+        position:'relative',
         width:'100px', 
         height:'100px',
         borderRadius:'50%',
         backgroundColor:'green',
         marginLeft:'40px'
+    },
+    paymentIconItem:{
+        fill: '#ffff',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        fontSize: 'xxx-large',
+        transform: 'translate(-50%, -50%)'
     }
 }));
 
@@ -35,8 +45,8 @@ function PaymentSuccessPage(){
             <Header/>
             <Typography variant="h4" className={classes.header}>نتیجه ی پرداخت</Typography>
             <Grid item lg={8} md={10} sm ={10} xs={10} className={classes.container}>
-                <div className={classes.paymentIcon}></div>
-                <Typography variant="p" component="p" >نتیجه ی پرداخت</Typography>
+                <div className={classes.paymentIcon}><DoneIcon className={classes.paymentIconItem} /></div>
+                <Typography dir="rtl" variant="p" component="p" >با تشکر از پرداخت شما. سفارش شما ثبت شده و جهت هماهنگی ارسال با شما تماس گرفته خواهد شد.</Typography>
             </Grid>
         </div>
     )
