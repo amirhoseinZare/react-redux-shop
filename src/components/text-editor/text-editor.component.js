61066fdd49286a9e53144f7b3c5f20a8e87a9e12
amir <zareamirhussein@gmail.com>
2021-07-15 18:19:27 +0430

    adding deafult value for text editor

--- src/components/text-editor/text-editor.component.js
+++ src/components/text-editor/text-editor.component.js
@@ -1,12 +1,12 @@
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState, convertToRaw } from 'draft-js';
+import { EditorState, convertToRaw, ContentState, convertFromHTML } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import {useEffect, useState} from "react"
 import classes from "./text-editor.module.scss"
 
 function TextEditor(props){
     const toolbar = {
-        options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji', 'image', 'remove', 'history'],
+        options: ['inline', 'blockType', 'fontSize', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji',],
         inline: {
           inDropdown: false,
           className: undefined,
@@ -35,12 +35,6 @@ function TextEditor(props){
           component: undefined,
           dropdownClassName: undefined,
         },
-        fontFamily: {
-          options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana'],
-          className: undefined,
-          component: undefined,
-          dropdownClassName: undefined,
-        },
         list: {
           inDropdown: false,
           className: undefined,
@@ -62,18 +56,6 @@ function TextEditor(props){
           center: { icon: 'center', className: undefined },
           right: { icon: 'right', className: undefined },
           justify: { icon: 'justify', className: undefined },
-        },
-        colorPicker: {
-          icon: 'color',
-          className: undefined,
-          component: undefined,
-          popupClassName: undefined,
-          colors: ['rgb(97,189,109)', 'rgb(26,188,156)', 'rgb(84,172,210)', 'rgb(44,130,201)',
-            'rgb(147,101,184)', 'rgb(71,85,119)', 'rgb(204,204,204)', 'rgb(65,168,95)', 'rgb(0,168,133)',
-            'rgb(61,142,185)', 'rgb(41,105,176)', 'rgb(85,57,130)', 'rgb(40,50,78)', 'rgb(0,0,0)',
-            'rgb(247,218,100)', 'rgb(251,160,38)', 'rgb(235,107,86)', 'rgb(226,80,65)', 'rgb(163,143,132)',
-            'rgb(239,239,239)', 'rgb(255,255,255)', 'rgb(250,197,28)', 'rgb(243,121,52)', 'rgb(209,72,65)',
-            'rgb(184,49,47)', 'rgb(124,112,107)', 'rgb(209,213,216)'],
         },
         link: {
           inDropdown: false,
@@ -115,39 +97,16 @@ function TextEditor(props){
             height: 'auto',
             width: 'auto',
           },
-        },
-        image: {
-          icon: 'image',
-          className: undefined,
-          component: undefined,
-          popupClassName: undefined,
-          urlEnabled: true,
-          uploadEnabled: true,
-          alignmentEnabled: true,
-          uploadCallback: undefined,
-          previewImage: false,
-          inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
-          alt: { present: false, mandatory: false },
-          defaultSize: {
-            height: 'auto',
-            width: 'auto',
-          },
-        },
-        remove: { icon: 'eraser', className: undefined, component: undefined },
-        history: {
-          inDropdown: false,
-          className: undefined,
-          component: undefined,
-          dropdownClassName: undefined,
-          options: ['undo', 'redo'],
-          undo: { icon: 'undo', className: undefined },
-          redo: { icon: 'redo', className: undefined },
-        },
+        }
+        
     }
-    const [editorState, setEditorState] = useState({editorState:EditorState.createEmpty()})
+    const [editorState, setEditorState] = useState({editorState:EditorState.createWithContent(
+      ContentState.createFromBlockArray(
+        convertFromHTML(props.defaultText || '')
+      ))})
 
     useEffect(()=>{
-        const htmlValue = draftToHtml(convertToRaw(editorState.editorState.getCurrentContent()))
+        const htmlValue = draftToHtml(convertToRaw(editorState.editorState.getCurrentContent('hello')))
         const {handleChange} = props
         handleChange(htmlValue)
     }, [editorState])
