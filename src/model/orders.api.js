34cf88d5e2f0c8403df8d2630b7686256e585ee8
amir <zareamirhussein@gmail.com>
2021-07-24 21:33:34 +0430

    removing model

--- src/model/orders.api.js
+++
@@ -1,5 +0,0 @@
-import Service from "./http.api"
-
-const order = new Service('orders')
-
-export default order
--- src/model/products.model.js
+++
@@ -1,5 +0,0 @@
-import Service from "./http.api"
-
-const product = new Service('products')
-
-export default product
